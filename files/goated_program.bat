c7aa0df76767570e5730eeaf3a3fd0f9f6d0a47f
mech0dev <160847561+mech0dev@users.noreply.github.com>
2024-06-28 21:46:59 +0530

    Add files via upload

---
+++ files/goated_program.bat
@@ -0,0 +1,161 @@
+@echo off
+:impna
+title are my friend lets see
+set /p in_name= echo enter your name:
+if %in_name%==pavan (goto mc)
+if %in_name%==vighnesh (goto fok)
+if %in_name%==vinil (goto fok)
+if %in_name%==siddarth (goto fok)
+if %in_name%==vishnu (goto fok)
+if %in_name%==ephraim (goto fok)
+if %in_name%==srikar (goto fok)
+if %in_name%==ujwal (goto fok_2)
+if %in_name%==daiwik (goto fok)
+if %in_name%==geeth (goto fok)
+if %in_name%==abhiram (goto fok)
+if %in_name%==gaurav (goto goro)
+if %in_name%==ramchran (goto fok)
+if %in_name%==paavan (goto fok)
+if %in_name%==nushanth (goto me)
+if %in_name%==arya (goto fok)
+if %in_name%==nahan (goto fok)
+if %in_name%==anandhi (goto fok)
+if %in_name%==jashmitha (goto fok)
+if %in_name%==aryanjali (goto fok)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if %in_name%==slot (goto slot)
+if not %in_name%==pavan (goto sory)
+if not %in_name%==siddardh (goto sory)
+if not %in_name%==vinil (goto sory)
+if not %in_name%==vighnesh (goto sory)
+if not %in_name%==vishnu (goto sory)
+if not %in_name%==ephraim (goto sory)
+if not %in_name%==srikar (goto sory)
+if not %in_name%==ujwal (goto sory)
+if not %in_name%==daiwik (goto sory)
+if not %in_name%==geeth (goto sory)
+if not %in_name%==abhiram (goto sory)
+if not %in_name%==gaurav (goto sory)
+if not %in_name%==ramchran (goto sory)
+if not %in_name%==paavan (goto sory)
+if not %in_name%==nushanth (goto sory)
+if not %in_name%==arya (goto sory)
+if not %in_name%==nahan (goto sory)
+if not %in_name%==anandhi (goto sory)
+if not %in_name%==jashmitha (goto sory)
+if not %in_name%==aryanjali (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+if not %in_name%==slot (goto sory)
+:slot
+echo pls fill up the slot
+echo press any key to go to input name
+pause >null
+goto impna
+:fok
+title FOK YOU
+echo FOK YOU!!!!! %in_name% 
+timeout /t 1 /nobreak >null
+echo haha you did not expect that RIGHT except goro i know but hahhhhaahhaaaaaa
+timeout /t 4 /nobreak >null
+del null
+exit
+:mc
+cls
+color 1
+title heaven my creator is here
+echo hello my creator
+timeout /t 2 /nobreak >null
+color 2
+echo i am very happy to see you
+timeout /t 2 /nobreak >null
+color 3
+echo its been a long day
+timeout /t 1 /nobreak >null
+color 4
+echo what else i am just a program  
+timeout /t 2 /nobreak >null
+color 5
+echo killing myself in 
+timeout /t 1 /nobreak >null
+color 6
+echo 3..
+timeout /t 1 /nobreak >null
+color 7
+echo 2...
+timeout /t 1 /nobreak >null
+color 8 
+echo 1
+timeout /t 1 /nobreak >null
+del null
+exit
+:plt_fok
+title ok i did not do this on purpose you made me put you name
+echo hi %in_name%
+timeout /t 1 /nobreak >null
+echo ok fok you %in_name%
+echo bye 
+timeout /t 4 /nobreak >null
+del null
+exit
+:sory
+color 5
+title sorry for your inconvinice
+echo sorry for your inconvinince i thought you are my friend
+timeout /t 4 /nobreak >null
+del null
+exit
+:me
+echo hi nushanth how are you bye
+title hi
+timeout /t 5 /nobreak >null
+del null
+exit
+:fok_2
+color 4
+title Ujwal fok
+echo not found in list 
+timeout /t 2 /nobreak >null
+cls
+echo *connecting lsm*
+timeout /t 2 /nobreak >null
+echo *lsm connected*
+echo fu*k you %in_name%
+timeout /t 3 /nobreak >null
+del null
+exit
+:goro
+title goro i knew it 
+echo *clap*x3
+timeout /t 2 /nobreak >null
+echo FOK YOU!!!
+timeout /t 2 /nobreak >null
+echo byeee
+timeout /t 2 /nobreak >null
+del null
+exit 
